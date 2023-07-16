ef4107bc5fe5cec3c7ff46ee53210674f39abded
Anibal Rivera <79974791+anriverax@users.noreply.github.com>
2023-07-15 23:48:12 -0600

    Update and rename redis.interfaces.ts to redis.decorators.ts

--- redis.decorators.ts
+++ redis.decorators.ts
@@ -1,3 +1,4 @@
+import { Inject } from '@nestjs/common';
 import * as Redis from 'ioredis';
 
 export type IRedis = Redis.Redis;
@@ -5,3 +6,7 @@ export type IRedis = Redis.Redis;
 export interface RedisModuleOptions {
   config: Redis.RedisOptions & { url?: string };
 }
+
+export const InjectRedis = () => {
+  return Inject('REDIS_CONNECTION');
+};
